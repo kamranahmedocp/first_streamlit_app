c87d0e86c9eda19d19c21a15c4fb53c846f6acad
kamran <113388191+kamranahmedocp@users.noreply.github.com>
2022-09-19 10:11:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,9 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+
 # New Section to display fruityvice apli response
+streamlit.header('Fruitvice Fruit Advise!')
 
 import requests
 fruitvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
