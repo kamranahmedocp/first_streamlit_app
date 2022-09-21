b29c0be0a83de3b7c84bcc0c2e9d991c26f4fd21
kamran <113388191+kamranahmedocp@users.noreply.github.com>
2022-09-21 17:01:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,7 @@ if streamlit.button('Get Fruit Load List'):
     
 streamlit.stop()
 
+#
 #  Add a Second Text Entry Box: 
 #  Allow the end user to add a fruite to  the list:
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
