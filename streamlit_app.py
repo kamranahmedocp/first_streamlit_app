d8528a265fca6a65f8cea356b7744656432e0679
kamran <113388191+kamranahmedocp@users.noreply.github.com>
2022-09-21 16:47:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,7 @@ except URLError as e:
     streamlit.error()
   
 streamlit.stop()
-#
- #
+
 streamlit.header("The fruit load list contain:")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
